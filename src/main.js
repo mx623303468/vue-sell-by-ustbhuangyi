2d4759477f306295ac719bd974bd1ff49d3e6590
mx623303468 <623303468@qq.com>
2019-05-28 16:11:44 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './cube-ui'
+// import 'cube-ui/lib/style.css'
 
 Vue.config.productionTip = false
 
---
+++ src/views/Header/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="header">header</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
